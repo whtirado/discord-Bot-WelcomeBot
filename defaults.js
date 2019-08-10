fcd863bdbdb4cc2b02d7918336d3ac22017a0365
whtirado <whtirado@gmail.com>
2019-08-10 13:12:39 -0400

    Added comments to defaults.js

--- defaults.js
+++ defaults.js
@@ -1,3 +1,8 @@
+// Default role assigned to new member
 exports.defaultRole = 'Member';
+
+// Default channel to mention in welcome DM
 exports.defaultChannel = 'general';
+
+// Default channel to send member and display new member message
 exports.defaultWelcome = 'welcome';
